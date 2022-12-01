570253a6541e8fa99cb959eecc3a5df5b062208d
Brad Bondurant <brad.bondurant@duke.edu>
2022-12-01 07:57:53 -0500

    flake: need newer version of openocd for 64-bit ELFs
    also added capstone as an input for disassembly

--- flake.nix
+++ flake.nix
@@ -14,18 +14,28 @@
       rustPlatform = zynq-rs.rustPlatform;
       cargo-xbuild = zynq-rs.packages.x86_64-linux.cargo-xbuild;
       mkbootimage = zynq-rs.packages.x86_64-linux.mkbootimage;
+
+      openocd = pkgs.openocd.overrideAttrs(oa: rec {
+        version = "0.12.0-rc2";
+        src = builtins.fetchurl {
+          url = "https://sourceforge.net/projects/${oa.pname}/files/${oa.pname}/${version}/${oa.pname}-${version}.tar.bz2";
+          sha256 = "17cfd2428c9abbbbf668ab6134dfac4ce4fb9454550f2b22104855a020a8261f";
+        };
+        patches = [];
+        buildInputs = oa.buildInputs ++ [ pkgs.capstone ];
+      });
     in
     {
       devShells.x86_64-linux.default = pkgs.mkShell {
         name = "zynq-us-rs-dev-shell";
         buildInputs = (with pkgs; [
-          openocd
           gdb
         ]) ++ [
           rustPlatform.rust.rustc
           rustPlatform.rust.cargo
           cargo-xbuild
           mkbootimage
+          openocd
         ];
       };
 
