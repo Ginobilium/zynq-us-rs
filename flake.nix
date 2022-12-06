2ad46e1e62ee2158cf5d2b4e5bc9468d2dd1a95c
Brad Bondurant <brad.bondurant@duke.edu>
2022-12-06 08:59:43 -0500

    flake: add gnu tools

--- flake.nix
+++ flake.nix
@@ -4,23 +4,201 @@
   inputs = {
     zynq-rs.url = git+https://git.m-labs.hk/M-Labs/zynq-rs;
     nixpkgs.follows = "zynq-rs/nixpkgs";
+    nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     mozilla-overlay.follows = "zynq-rs/mozilla-overlay";
+    binutils-src = {
+      type = "tarball";
+      url = "https://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.bz2";
+      narHash = "sha256-ksj3VaJMrb82zUgEVSHW/agX4X8q9hgUrywHCNz9ENk=";
+      flake = false;
+    };
+    gcc-src = {
+      type = "tarball";
+      url = "https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz";
+      narHash = "sha256-u1AF7a7xtXdepsfbvw2+501EF88rOFVMOZX3DUFf/9I=";
+      flake = false;
+    };
+    gdb-src = {
+      type = "tarball";
+      url = "https://ftp.gnu.org/gnu/gdb/gdb-12.1.tar.xz";
+      narHash = "sha256-tyR2yqsOQupCC3csq0W41vntPhjJ0Wqx//8O0veE1M4=";
+      flake = false;
+    };
+    newlib-src = {
+      type = "tarball";
+      url = "ftp://sourceware.org/pub/newlib/newlib-4.2.0.20211231.tar.gz";
+      narHash = "sha256-nJtCMMkSPoWnS5CTvlOalydfCMk906Oq4bUT37sl99I=";
+      flake = false;
+    };
+    openocd-src = {
+      type = "tarball";
+      url = "https://sourceforge.net/projects/openocd/files/openocd/0.12.0-rc2/openocd-0.12.0-rc2.tar.bz2";
+      narHash = "sha256-xwi6Y7aglrLo0p1TAwCGU+ApNgNRXnJcffrWZc5RdwE=";
+      flake = false;
+    };
   };
 
-  outputs = { self, zynq-rs, nixpkgs, mozilla-overlay }:
+  outputs = { self, zynq-rs, nixpkgs, nixpkgs-unstable, mozilla-overlay, binutils-src, gcc-src, gdb-src, newlib-src, openocd-src }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; overlays = [ (import mozilla-overlay) ]; };
+      pkgs-unstable = import nixpkgs-unstable { system = "x86_64-linux"; };
 
       rustPlatform = zynq-rs.rustPlatform;
       cargo-xbuild = zynq-rs.packages.x86_64-linux.cargo-xbuild;
       mkbootimage = zynq-rs.packages.x86_64-linux.mkbootimage;
 
+      gnu-platform = "aarch64-none-elf";
+
+      binutils-pkg = { zlib, bison, perl, texinfo, gettext, extraConfigureFlags ? [] }: pkgs.stdenv.mkDerivation rec {
+        basename = "binutils";
+        pname = "${basename}-${gnu-platform}";
+        version = "2.39";
+        src = binutils-src;
+        configureFlags = [
+          "--enable-deterministic-archives"
+          "--with-system-zlib"
+          "--target=${gnu-platform}"
+          "--with-cpu=cortex-a53"
+          "--with-fpu=vfpv3"
+          "--with-float=hard"
+          "--with-mode=thumb"
+        ] ++ extraConfigureFlags;
+        outputs = [ "out" "info" "man" ];
+        depsBuildBuild = [ pkgs.buildPackages.stdenv.cc ];
+        nativeBuildInputs = [ bison perl texinfo ];
+        buildInputs = [ zlib gettext ];
+        enableParallelBuilding = true;
+        meta = {
+          description = "Tools for manipulating binaries (linker, assembler, etc.)";
+          longDescription = ''
+            The GNU Binutils are a collection of binary tools.  The main
+            ones are `ld' (the GNU linker) and `as' (the GNU assembler).
+            They also include the BFD (Binary File Descriptor) library,
+            `gprof', `nm', `strip', etc.
+          '';
+          homepage = http://www.gnu.org/software/binutils/;
+          license = pkgs.lib.licenses.gpl3Plus;
+          /* Give binutils a lower priority than gcc-wrapper to prevent a
+            collision due to the ld/as wrappers/symlinks in the latter. */
+          priority = "10";
+        };
+      };
+
+      gcc-pkg = { gmp, mpfr, libmpc, platform-binutils, extraConfigureFlags ? [] }: pkgs.stdenv.mkDerivation rec {
+        basename = "gcc";
+        pname = "${basename}-${gnu-platform}";
+        version = "12.2.0";
+        src = gcc-src;
+        preConfigure = ''
+          mkdir build
+          cd build
+        '';
+        configureScript = "../configure";
+        configureFlags = [ 
+          "--target=${gnu-platform}"
+          "--with-arch=armv8-a"
+          "--with-tune=cortex-a53"
+          "--disable-libssp"
+          "--enable-languages=c"
+          "--with-as=${platform-binutils}/bin/${gnu-platform}-as"
+          "--with-ld=${platform-binutils}/bin/${gnu-platform}-ld" ] ++ extraConfigureFlags;
+        outputs = [ "out" "info" "man" ];
+        hardeningDisable = [ "format" "pie" ];
+        propagatedBuildInputs = [ gmp mpfr libmpc platform-binutils ];
+        enableParallelBuilding = true;
+        dontFixup = true;
+      };
+
+      gdb-pkg = { 
+        pkg-config, texinfo, perl, setupDebugInfoDirs, 
+        ncurses, readline, gmp, mpfr, expat, libipt, zlib, guile, sourceHighlight,
+      }: pkgs.stdenv.mkDerivation rec {
+        basename = "gdb";
+        pname = "${basename}-${gnu-platform}";
+        version = "12.1";
+        src = gdb-src;
+
+        nativeBuildInputs = [ pkg-config texinfo perl setupDebugInfoDirs ];
+        buildInputs = [ ncurses readline gmp mpfr expat libipt zlib guile sourceHighlight ];
+        propagatedNativeBuildInputs = [ setupDebugInfoDirs ];
+        depsBuildBuild = [ pkgs.buildPackages.stdenv.cc ];
+
+        preConfigure = ''
+          mkdir _build
+          cd _build
+        '';
+        configureScript = "../configure";
+        configureFlags = [
+          "--disable-werror"
+          "--disable-install-libbfd"
+          "--disable-shared"
+          "--enable-static"
+          "--with-system-zlib"
+          "--with-system-readline"
+          "--enable-tui"
+          "--with-curses"
+          "--with-gmp=${gmp.dev}"
+          "--with-mpfr=${mpfr.dev}"
+          # "--with-guile=${guile.dev}"
+          "--with-expat" 
+          "--with-libexpat-prefix=${expat.dev}"
+          "--target=${gnu-platform}"
+          "--with-arch=armv8-a"
+          "--with-tune=cortex-a53"
+        ];
+        postInstall = '' 
+          # Remove Info files already provided by Binutils and other packages.
+          rm -v $out/share/info/bfd.info
+        '';
+        enableParallelBuilding = true;
+      };
+
+      newlib-pkg = { platform-binutils, platform-gcc }: pkgs.stdenv.mkDerivation rec {
+        pname = "newlib";
+        version = "4.2.0.20211231";
+        src = newlib-src;
+        nativeBuildInputs = [ platform-binutils platform-gcc ];
+        configureFlags = [
+          "--target=${gnu-platform}"
+
+          "--with-cpu=cortex-a53"
+          # "--with-fpu=vfpv3"
+          "--with-float=hard"
+          "--with-mode=thumb"
+          "--enable-interwork"
+          "--disable-multilib"
+
+          "--disable-newlib-supplied-syscalls"
+          "--with-gnu-ld"
+          "--with-gnu-as"
+          "--disable-newlib-io-float"
+          "--disable-werror"
+        ];
+        dontFixup = true;
+      };
+      gnutoolchain = rec {
+        binutils-bootstrap = pkgs.callPackage binutils-pkg { };
+        gcc-bootstrap = pkgs.callPackage gcc-pkg {
+          platform-binutils = binutils-bootstrap;
+          extraConfigureFlags = [ "--disable-libgcc" ];
+        };
+        newlib = pkgs.callPackage newlib-pkg {
+          platform-binutils = binutils-bootstrap;
+          platform-gcc = gcc-bootstrap;
+        };
+        binutils = pkgs.callPackage binutils-pkg {
+          extraConfigureFlags = [ "--with-lib-path=${newlib}/arm-none-eabi/lib" ];
+        };
+        gcc = pkgs.callPackage gcc-pkg {
+          platform-binutils = binutils;
+          extraConfigureFlags = [ "--enable-newlib" "--with-headers=${newlib}/arm-none-eabi/include" ];
+        };
+        gdb = pkgs.callPackage gdb-pkg { readline = pkgs.readline81; };
+      };
+
       openocd = pkgs.openocd.overrideAttrs(oa: rec {
         version = "0.12.0-rc2";
-        src = builtins.fetchurl {
-          url = "https://sourceforge.net/projects/${oa.pname}/files/${oa.pname}/${version}/${oa.pname}-${version}.tar.bz2";
-          sha256 = "17cfd2428c9abbbbf668ab6134dfac4ce4fb9454550f2b22104855a020a8261f";
-        };
+        src = openocd-src;
         patches = [];
         buildInputs = oa.buildInputs ++ [ pkgs.capstone ];
       });
@@ -30,9 +208,13 @@
         name = "zynq-us-rs-dev-shell";
         buildInputs = (with pkgs; [
           gdb
+          llvmPackages_9.clang-unwrapped
         ]) ++ [
           rustPlatform.rust.rustc
           rustPlatform.rust.cargo
+          gnutoolchain.binutils
+          gnutoolchain.gcc
+          gnutoolchain.gdb
           cargo-xbuild
           mkbootimage
           openocd
