6fa9e91ed0a97043b14e86a56922e1ac95952e76
Brad Bondurant <brad.bondurant@duke.edu>
2022-12-06 09:03:22 -0500

    test: reject modernity, return to assembly

--- test/src/main.rs
+++ test/src/main.rs
@@ -5,7 +5,7 @@
 // Original author: Astro
 #![no_std]
 #![no_main]
-#![feature(asm, llvm_asm)]
+#![feature(asm)]
 #![feature(naked_functions)]
 // #![feature(compiler_builtins_lib)]
 // #![feature(never_type)]
@@ -37,22 +37,27 @@ extern "C" {
 #[no_mangle]
 #[naked]
 pub unsafe extern "C" fn _boot_cores() -> ! {
-    match MPIDREL1.read().cpu_id() {
-        0 => {
-            SP.write(&mut __stack0_start as *mut _ as u64);
-            boot_core0();
-        }
-        _ => loop {
-            // if not core0, infinitely wait for events
-            asm::wfe();
-        },
-    }
+    asm!(
+        "mrs x0, mpidr_el1",  // read MPIDR_EL1
+        "and x0, x0, #0xff",  // get CPU ID within cluster (0-3)
+        // TODO: configure SPs for other cores. For now just worry about core 0.
+        "cbnz x0, 0f",
+        // core 0
+        "ldr x1, =__stack0_start",
+        "mov sp, x1",
+        "bl boot_core0",
+        // cores 1-3
+        "0:",
+        "wfe",
+        "b 0b",
+        options(noreturn)
+    );
 }
 
-#[naked]
+#[no_mangle]
 #[inline(never)]
 unsafe fn boot_core0() -> ! {
-    cache_init();
+    // cache_init();
     zero_bss(&mut __bss_start, &mut __bss_end);
     main();
     panic!("return from main")
